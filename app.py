a41afe252ec3b1097ed150cf1a506395f2f55017
Oleg Mayorov <68591639+mayor-oleg@users.noreply.github.com>
2021-04-05 21:52:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@
 from flask import Flask, Response,render_template
 
 app = Flask(__stream__)
-#server = app.server
+server = app.server
 
 @app.route('/')
 def index():
@@ -17,5 +17,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run_server()
 
