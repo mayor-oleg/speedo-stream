67773bf06e73a6926263dd5f73c7a226b5cb946b
Oleg Mayorov <68591639+mayor-oleg@users.noreply.github.com>
2021-04-06 20:03:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,40 @@ from flask import Flask, Response,render_template
 app = Flask(__name__)
 #server = app.server
 
+
+
+@app.route('/audio')
+def audio():
+    # start Recording
+    def sound():
+
+        sampleRate = fs = 44100  # Частота дискретизации
+        seconds = 3  # Продолжительность записи
+
+        bitsPerSample = 16
+        channels = 2
+        wav_header = genHeader(sampleRate, bitsPerSample, channels)
+
+        stream = myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+        sd.wait()  # Дождитесь окончания записи
+        #write('output.wav', fs, myrecording)  # Сохранить как WAV файл
+        print("recording...")
+        #frames = []
+        first_run = True
+        while True:
+           if first_run:
+               data = wav_header + stream
+               first_run = False
+           else:
+               data = stream
+           yield(data)
+
+    return Response(sound())
+
+
+
+
+
 @app.route('/')
 def index():
     """Video streaming home page."""
